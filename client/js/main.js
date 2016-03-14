0bfe3f0c81915a2773234a6439be6b67fc906f1a
Chase Brasfield <chasebrasfield@gmail.com>
2016-03-14 11:13:19 -0500

    get and post both work now

--- client/js/main.js
+++ client/js/main.js
@@ -1,31 +1,46 @@
 $(document).ready(function() {
-    console.log('ready');
-
-    $('#button').click(function() {
-        var post = $('#newtweet').val();
-        console.log(post);
-    });
-
-    function postData() {
-
-    }
-
     function getData() {
         $.ajax({
             method: 'GET',
             url: 'messages',
         }).done(function(list) {
             var array = list.split('\n');
-            console.log(array);
+            //console.log(array);
             for (i = 0; i < array.length; i++) {
                 var tweet = array[i];
-                console.log(tweet);
+                //console.log(tweet);
                 var parsedTweet = JSON.parse(tweet);
-                console.log(parsedTweet);
+                //console.log(parsedTweet);
                 var finalTweet = "USER: " + parsedTweet.text;
-                $('.incoming-tweets').prepend('<li>'+finalTweet+'</li>')
+                $('.incoming-tweets').prepend('<li>' + finalTweet + '</li>')
             };
         });
     };
     getData();
 });
+
+$('#button').click(function() {
+    if ($('#new-tweet').val().length === 0) {
+        alert('You did not enter a tweet to post!');
+        return;
+    } else {
+        postData();
+    }
+
+    });
+
+function postData() {
+    var user = 'USER: '
+    var incTweet = {
+        text: $('#new-tweet').val(),
+        userName: user
+    };
+    $.ajax({
+        method: 'POST',
+        url: 'messages',
+
+    }).then(function() {
+        $('.incoming-tweets').prepend('<li>' + user + incTweet.text + '</li>')
+    });
+    $('#new-tweet').val('');
+}
